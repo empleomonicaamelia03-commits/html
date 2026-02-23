aa05083926604835e712fbf4fab20c1f5c810fc7
Monica Amelia <empleo.monicaamelia03@gmail.com>
2026-02-23 22:41:36 +0800

    add file

---
+++ myScript.js
@@ -0,0 +1,12 @@
+const div1 = document.getElementById("about");
+const div2 = document.getElementById("education");
+const div3 = document.getElementById("portfolio");
+const div4 = document.getElementById("skills");
+const div5 = document.getElementById("contact");
+
+//console logs
+console.log("About Section:", div1);
+console.log("Education Section:", div2);
+console.log("Portfolio Section:", div3);
+console.log("Skills Section:", div4);
+console.log("Contact Section:", div5);
